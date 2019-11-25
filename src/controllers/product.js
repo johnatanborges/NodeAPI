8d378b1df7899932fbded827ca0d3778733ca1e2
Johnatan Borges <johnatanborges@hotmail.com>
2019-11-25 10:27:15 -0300

    Get Slug Route

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -2,6 +2,24 @@ const mongoose = require('mongoose')
 const Product = mongoose.model('Product')
 
 
+exports.get = (req, res, next) => {
+    Product.find({ active: true }, 'title price slug')
+        .then((products) => {
+            res.status(200).send(products)
+        }).catch(error => {
+            res.status(400).send(error)
+        })
+}
+
+exports.getBySlug = (req, res, next) => {
+    Product.findOne({ slug: req.params.slug }, 'title description price slug tags')
+        .then((products) => {
+            res.status(200).send(products)
+        }).catch(error => {
+            res.status(400).send(error)
+        })
+}
+
 exports.post = (req, res, next) => {
     const product = new Product(req.body)
 
--- src/routes/product.js
+++ src/routes/product.js
@@ -2,10 +2,14 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/product')
 
+router.get('/products', controller.get)
+
+router.get('/products/:slug', controller.getBySlug)
+
 router.post('/products', controller.post)
 
 router.put('/products/:id', controller.put)
 
-router.delete('/products', controller.delete)
+router.delete('/products/:id', controller.delete)
 
 module.exports = router
